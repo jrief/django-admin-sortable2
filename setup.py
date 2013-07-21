9ffe59968d697bdd94eaba353be984c7be33519c
Jacob Rief <jacob.rief@gmail.com>
2013-07-21 16:37:02 +0200

    excluding exampke and docs

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=['Django>=1.4'],
-    packages=find_packages(),
+    packages=find_packages(exclude=["example", "docs"]),
     include_package_data=True,
 )
