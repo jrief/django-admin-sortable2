ea9accce428ab8fe0f8c728933134f69bdfe4e34
Jacob Rief <jacob.rief@gmail.com>
2013-08-28 16:49:33 +0200

    Added external relationship to author

--- example/testapp/admin.py
+++ example/testapp/admin.py
@@ -1,10 +1,15 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
 from adminsortable.admin import SortableAdminMixin
-from models import SortableBook
+from models import Author, SortableBook
 
 
 class SortableBookAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_per_page = 15
-    list_display = ('title',)
+    list_display = ('title', 'order',)
 admin.site.register(SortableBook, SortableBookAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+admin.site.register(Author, AuthorAdmin)
--- example/testapp/models.py
+++ example/testapp/models.py
@@ -2,9 +2,17 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField('Name', null=True, blank=True, max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+
 class SortableBook(models.Model):
     title = models.CharField('Title', null=True, blank=True, max_length=255)
     order = models.PositiveIntegerField(blank=False, null=False)
+    author = models.ForeignKey(Author, null=True)
 
     class Meta(object):
         ordering = ['order']
