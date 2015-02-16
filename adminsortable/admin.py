85fcc91051ffa69ffcf06c0de7cd967b6db3053a
Tino de Bruijn <tinodb@gmail.com>
2015-02-16 20:07:13 +0100

    Update from admin.ModelAdmin.queryset to admin.ModelAdmin.get_queryset

--- adminsortable/admin.py
+++ adminsortable/admin.py
@@ -83,7 +83,7 @@ class SortableAdminMixin(SortableAdminBase):
 
     def get_actions(self, request):
         actions = super(SortableAdminMixin, self).get_actions(request)
-        paginator = self.get_paginator(request, self.queryset(request), self.list_per_page)
+        paginator = self.get_paginator(request, self.get_queryset(request), self.list_per_page)
         if len(paginator.page_range) > 1:
             # add actions for moving items to other pages
             move_actions = []
