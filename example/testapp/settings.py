5d45bf909eab5638001ac7e59daa2ded967184b6
Tino de Bruijn <tinodb@gmail.com>
2015-02-16 21:01:53 +0100

    Add MIDDLEWARE_CLASSES setting for tests on django>=1.7

--- example/testapp/settings.py
+++ example/testapp/settings.py
@@ -53,3 +53,9 @@ INSTALLED_APPS = (
     'adminsortable',
     'testapp',
 )
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
