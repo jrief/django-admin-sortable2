1441aea7991d0d16d0dfaca58619603723f2aeb2
Jacob Rief <jacob.rief@gmail.com>
2015-12-01 23:26:05 +0100

    Fixes #76

--- adminsortable2/management/commands/reorder.py
+++ adminsortable2/management/commands/reorder.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
-from django.utils.module_loading import import_by_path
-from django.core import exceptions
+from django.utils.module_loading import import_string
 
 
 class Command(BaseCommand):
@@ -11,12 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for modelname in args:
             try:
-                Model = import_by_path(modelname)
-            except exceptions.ImproperlyConfigured:
+                Model = import_string(modelname)
+            except ImportError:
                 raise CommandError('Unable to load model "%s"' % modelname)
             if not hasattr(Model._meta, 'ordering') or len(Model._meta.ordering) == 0:
                 raise CommandError('Model "{0}" does not define field "ordering" in its Meta class'.format(modelname))
             orderfield = Model._meta.ordering[0]
+            if orderfield[0] == '-':
+                orderfield = orderfield[1:]
             for order, obj in enumerate(Model.objects.iterator(), start=1):
                 setattr(obj, orderfield, order)
                 obj.save()
