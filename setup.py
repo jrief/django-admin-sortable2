ab8d6010cafb53d0ede460e1ff1e82a1a197351d
Anatoliy Basov <demndell@gmail.com>
2016-10-11 19:23:43 +0700

    Specific versions of python

--- setup.py
+++ setup.py
@@ -24,6 +24,10 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
 ]
 
 
