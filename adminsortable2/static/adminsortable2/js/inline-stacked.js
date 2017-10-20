100cf6e3a5bb6452a3632b844c12ccda7209b704
Sander van Leeuwen <sanderl@mediamonks.com>
2017-10-20 12:33:53 +0200

    Fix inline-stacked config parsing by using correct attribute

--- adminsortable2/static/adminsortable2/js/inline-stacked.js
+++ adminsortable2/static/adminsortable2/js/inline-stacked.js
@@ -1,7 +1,7 @@
 django.jQuery(function ($) {
 	$("script.inline-stacked-config").each(function(i, config_element) {
 		try {
-			var config = JSON.parse(config_element.text());
+			var config = JSON.parse(config_element.textContent);
 			$("#"+ config["prefix"] + "-group .inline-related").stackedFormset(config);
 		}
 		catch (parse_error) {
