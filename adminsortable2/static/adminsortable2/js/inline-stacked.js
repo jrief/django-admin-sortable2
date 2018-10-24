635d7299a8f0c71420b02ab43ecbd44c7d91a252
Pi Delport <pjdelport@gmail.com>
2018-10-24 14:34:36 +0200

    Inline helpers: Move Django formset call out of try block
    This avoids incorrectly reporting errors raised from the Django formset
    calls as invalid configuration / JSON parse errors.

--- adminsortable2/static/adminsortable2/js/inline-stacked.js
+++ adminsortable2/static/adminsortable2/js/inline-stacked.js
@@ -2,10 +2,10 @@ django.jQuery(function($) {
 	$("script.inline-stacked-config").each(function (i, config_element) {
 		try {
 			var config = JSON.parse(config_element.textContent);
-			$("#" + config["prefix"] + "-group .inline-related").stackedFormset($('this').selector, config);
 		}
 		catch (parse_error) {
 			console.error("Configuration for a django-adminsortable2 inline-stacked form is invalid.");
 		}
+		$("#" + config["prefix"] + "-group .inline-related").stackedFormset($('this').selector, config);
 	});
 });
