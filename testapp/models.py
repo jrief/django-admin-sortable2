aed59ded542acc1c7a823c057484a2acdca44361
Jacob Rief <jacob.rief@uibk.ac.at>
2022-03-31 13:07:34 +0200

    drop Python-2 legacies

--- testapp/models.py
+++ testapp/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField("Name", null=True, blank=True, max_length=255)
+    name = models.CharField(
+        "Name",
+        null=True,
+        blank=True,
+        max_length=255,
+    )
 
     class Meta:
         ordering = ['name']
@@ -10,62 +15,84 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-    def __unicode__(self):
-        return self.name
-
 
 class SortableBook(models.Model):
-    title = models.CharField("Title", null=True, blank=True, max_length=255)
-    my_order = models.PositiveIntegerField(default=0, blank=False, null=False)
-    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    title = models.CharField(
+        "Title",
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    my_order = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+        db_index=True,
+    )
+
+    author = models.ForeignKey(
+        Author,
+        null=True,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
-        return self.title
-
-    def __unicode__(self):
         return self.title
 
 
 class SortableBook1(SortableBook):
     class Meta:
         proxy = True
-        verbose_name = "Book 1"
-        verbose_name_plural = "Books 1"
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
 
 
 class SortableBook2(SortableBook):
     class Meta:
         proxy = True
-        verbose_name = "Book 2"
-        verbose_name_plural = "Books 2"
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
 
 
 class SortableBook3(SortableBook):
     class Meta:
         proxy = True
         ordering = ['my_order']
-        verbose_name = "Book 3"
-        verbose_name_plural = "Books 3"
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
 
 
 class SortableBook4(SortableBook):
     class Meta:
         proxy = True
         ordering = ['-my_order']
-        verbose_name = "Book 4"
-        verbose_name_plural = "Books 4"
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
 
 
 class Chapter(models.Model):
-    title = models.CharField('Title', null=True, blank=True, max_length=255)
-    book = models.ForeignKey(SortableBook, null=True, on_delete=models.CASCADE)
-    my_order = models.PositiveIntegerField(blank=False, null=False, db_index=True)
+    title = models.CharField(
+        "Title",
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    book = models.ForeignKey(
+        SortableBook,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    my_order = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+        db_index=True,
+    )
 
     class Meta:
         ordering = ['my_order']
 
     def __str__(self):
-        return 'Chapter: {0}'.format(self.title)
-
-    def __unicode__(self):
-        return 'Chapter: {0}'.format(self.title)
+        return "Chapter: {0}".format(self.title)
