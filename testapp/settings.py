615c6edeba08c0a0f15cc8a171df540be75e2f28
Jacob Rief <jacob.rief@gmail.com>
2022-06-09 18:02:45 +0200

    use separate database files for manual- and unit-testing

--- testapp/settings.py
+++ testapp/settings.py
@@ -22,7 +22,13 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(Path(__file__).parent / 'test.db'),  # live_server requires a file rather than :memory:
+        'NAME': Path(__file__).parent / 'demo.sqlite3',
+        'TEST': {
+            'NAME': Path(__file__).parent / 'test.sqlite3',  # live_server requires a file rather than :memory:
+            'OPTIONS': {
+                'timeout': 20,
+            },
+        },
     }
 }
 
