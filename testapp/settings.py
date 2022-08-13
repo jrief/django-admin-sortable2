30ae8b66380f9d483b55c73080d01e6020c67b2a
hiaselhans <simon.klemenc@gmail.com>
2022-08-13 09:52:20 +0200

    add jinja template engine (#320)

--- testapp/settings.py
+++ testapp/settings.py
@@ -53,6 +53,18 @@ SILENCED_SYSTEM_CHECKS = ['admin.E408']
 STATIC_URL = '/static/'
 
 TEMPLATES = [{
+    'BACKEND': 'django.template.backends.jinja2.Jinja2',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+}, {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [],
     'APP_DIRS': True,
