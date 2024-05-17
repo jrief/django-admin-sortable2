5ea4352958ad85b98763f3ea889f8e91953b1c0d
Jacob Rief <jacob.rief@gmail.com>
2024-05-17 09:07:18 +0200

    Fix: loading the unminimized JavaScript files failed with DEBUG=True

--- client/build.cjs
+++ client/build.cjs
@@ -1,13 +1,13 @@
 const { build } = require('esbuild');
 const buildOptions = require('yargs-parser')(process.argv.slice(2), {
-  boolean: ['debug'],
+  boolean: ['debug', 'sourcemap'],
 });
 
 build({
   entryPoints: ['client/admin-sortable2.ts'],
   bundle: true,
   minify: !buildOptions.debug,
-  sourcemap: buildOptions.debug,
+  sourcemap: buildOptions.sourcemap,
   outfile: 'adminsortable2/static/adminsortable2/js/adminsortable2' + (buildOptions.debug ? '' : '.min') + '.js',
   plugins: [],
   target: ['es2020', 'chrome84', 'firefox84', 'safari14', 'edge84']
