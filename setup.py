c7a4ad2e7344635f9319bfd29cd1284324d0d4b7
Jacob Rief <jacob.rief@gmail.com>
2025-05-14 15:48:07 +0200

    use Node 20 for building

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Framework :: Django',
     'Framework :: Django :: 4.2',
     'Framework :: Django :: 5.0',
